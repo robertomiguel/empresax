2dcc3ffdd48e5412de90afe608d03735c560509a
Roberto Virtual <robertomiguel@outlook.com>
2016-02-29 20:43:17 -0300

    menu auto ok

--- app/views/admin/inicio.blade.php
+++ app/views/admin/inicio.blade.php
@@ -1,14 +1,14 @@
 @extends ('cabecera')
 
 @section ('content') 
-  <script>
+<script>
   $(function() {
     $( "#menu" ).menu();
   });
-  </script>
-  <style>
+</script>
+<style>
   .ui-menu { width: 250px; }
-  </style>
+</style>
 <?php 
 /*
 $sql = "insert into menu_general (nombre, id_padre, id_grupos,visible)
@@ -16,50 +16,88 @@ values ('listado esp 1', 15, '0', 1)";
 $datos = DB::insert($sql);
 */
 
-$sql = "select * from menu_general order by id_padre, id";
+$sql   = "select * from menu_general order by id_padre, id";
 $datos = DB::select($sql);
+$menu1 = 0;
+$menu2 = 0;
+$menu3 = 0;
+$menu4 = 0;
 ?>
 <pre>
 @foreach ($datos as $m)
 ID:{{$m->id}} NOMBRE:{{$m->nombre}} ID_PADRE:{{$m->id_padre}}
 @endforeach	
 </pre>
+
 <ul id="menu">
 @foreach ($datos as $m)
 
 	@if ($m->id_padre == 0)
-	<li>Modulo: {{$m->nombre}}
-	<ul>
-	@foreach ($datos as $sm)
-	@if ($sm->id_padre == $m->id && $m->id <> 0)
-		<li>SUB (1) id={{$sm->id}} / id_padre={{$sm->id_padre}}: {{$sm->nombre}}
-	<ul>
-	@foreach ($datos as $sm2)
-	@if ($sm2->id_padre == $sm->id && $sm->id <> 0)
-			<li>SUB (2) id={{$sm2->id}} / id_padre={{$sm2->id_padre}}: {{$sm2->nombre}}
-	<ul>
-	@foreach ($datos as $sm3)
-	@if ($sm3->id_padre == $sm2->id && $sm2->id <> 0)
-				<li>SUB (3) id={{$sm3->id}} / id_padre={{$sm3->id_padre}}: {{$sm3->nombre}}
-	<ul>
-	@foreach ($datos as $sm4)
-	@if ($sm4->id_padre == $sm3->id && $sm3->id <> 0)
-					<li>SUB (4) id={{$sm4->id}} / id_padre={{$sm4->id_padre}}: {{$sm4->nombre}}</li>
-	@endif
-	@endforeach
-	</ul>
-	@endif
-	@endforeach
-	</li></ul>
-	@endif
-	@endforeach
-	</li></ul>
-	@endif
-	@endforeach
-	</li></ul>
+		<li>Modulo: {{$m->nombre}}
+		@foreach ($datos as $sm)
+			@if ($sm->id_padre == $m->id && $m->id <> 0)
+				@if ($menu1==0)
+					<ul>
+					<?php $menu1=1; ?>
+				@endif
+				<li>SUB (1) id={{$sm->id}} / id_padre={{$sm->id_padre}}: {{$sm->nombre}}
+
+				@foreach ($datos as $sm2)
+					@if ($sm2->id_padre == $sm->id && $sm->id <> 0)
+						@if ($menu2==0)
+							<ul>
+							<?php $menu2=1; ?>
+						@endif
+						<li>SUB (1) id={{$sm2->id}} / id_padre={{$sm2->id_padre}}: {{$sm2->nombre}}
+
+						@foreach ($datos as $sm3)
+							@if ($sm3->id_padre == $sm2->id && $sm2->id <> 0)
+								@if ($menu3==0)
+									<ul>
+									<?php $menu3=1; ?>
+								@endif
+								<li>SUB (1) id={{$sm3->id}} / id_padre={{$sm3->id_padre}}: {{$sm3->nombre}}
+
+								@foreach ($datos as $sm4)
+									@if ($sm4->id_padre == $sm3->id && $sm3->id <> 0)
+										@if ($menu4==0)
+											<ul>
+											<?php $menu4=1; ?>
+										@endif
+										<li>SUB (1) id={{$sm4->id}} / id_padre={{$sm4->id_padre}}: {{$sm4->nombre}}
+									@endif
+								@endforeach
+									@if ($menu4==1)
+										</ul>
+										<?php $menu4=0; ?>
+									@endif
+								</li>
+
+							@endif
+						@endforeach
+							@if ($menu3==1)
+								</ul>
+								<?php $menu3=0; ?>
+							@endif
+						</li>
+
+					@endif
+				@endforeach
+					@if ($menu2==1)
+						</ul>
+						<?php $menu2=0; ?>
+					@endif
+				</li>
+
+			@endif
+		@endforeach
+			@if ($menu1==1)
+				</ul>
+				<?php $menu1=0; ?>
+			@endif
+		</li>
 	@endif
 
 @endforeach
-</ul>
-
+</lu>
 @stop
